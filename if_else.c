9894bfe0fb2ba74598c529d4dad463063a793068
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-06 22:19:24 +0600

    Added another else if condition

--- if_else.c
+++ if_else.c
@@ -15,7 +15,10 @@ int main()
     {
         printf("As the amount of money is %d BDT, I will buy Tomato.", money);
     }
-
+    else if (money >= 20)
+    {
+        printf("As the amount of money is %d BDT, I will buy Chips.", money);
+    }
     else
     {
         printf("As the amount of money is %d BDT, I won't buy a burger", money);
