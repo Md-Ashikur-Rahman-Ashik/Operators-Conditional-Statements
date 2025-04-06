3e2f17d92baee11b6fc324f4009b3f07fffd430f
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-06 20:22:28 +0600

    Added multiplication operation

--- arithmetic.c
+++ arithmetic.c
@@ -2,16 +2,18 @@
 
 int main()
 {
-    int num1, num2, sum, substract;
+    int num1, num2, sum, substract, multiply;
 
     num1 = 10;
     num2 = 20;
 
     sum = num1 + num2;
     substract = num1 - num2;
+    multiply = num1 * num2;
 
     printf("The sum is : %d\n", sum);
-    printf("The difference is : %d", substract);
+    printf("The difference is : %d\n", substract);
+    printf("The multiplication is : %d\n", multiply);
 
     return 0;
 }
