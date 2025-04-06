27e7ee18b7ac1b6267cf18a5e7a93667c4705814
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-06 20:18:43 +0600

    Added substract operation

--- arithmetic.c
+++ arithmetic.c
@@ -2,14 +2,16 @@
 
 int main()
 {
-    int num1, num2, sum;
+    int num1, num2, sum, substract;
 
     num1 = 10;
     num2 = 20;
 
     sum = num1 + num2;
+    substract = num1 - num2;
 
-    printf("The sum is : %d", sum);
+    printf("The sum is : %d\n", sum);
+    printf("The difference is : %d", substract);
 
     return 0;
 }
