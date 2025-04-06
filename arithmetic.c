2110977268feddad5b955a67535fb66381752527
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-06 20:24:26 +0600

    Added division operation

--- arithmetic.c
+++ arithmetic.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int num1, num2, sum, substract, multiply;
+    int num1, num2, sum, substract, multiply, divide;
 
     num1 = 10;
     num2 = 20;
@@ -10,10 +10,12 @@ int main()
     sum = num1 + num2;
     substract = num1 - num2;
     multiply = num1 * num2;
+    divide = num1 / num2;
 
     printf("The sum is : %d\n", sum);
     printf("The difference is : %d\n", substract);
     printf("The multiplication is : %d\n", multiply);
+    printf("The division is : %d", divide);
 
     return 0;
 }
