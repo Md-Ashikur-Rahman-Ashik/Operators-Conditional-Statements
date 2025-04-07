eb16427c891a1f33d7beb32bcbb35a04360526ec
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-07 06:39:58 +0600

    Solving practice problem

---
+++ practice_problem.c
@@ -0,0 +1,26 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     double num1 = 15 / 4;
+//     printf("%lf", num1);
+
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 6;
+
+    if (num1 > 5)
+    {
+        printf("Hi");
+    }
+    else if (num1 > 3)
+    {
+        printf("Bye");
+    }
+    return 0;
+}
